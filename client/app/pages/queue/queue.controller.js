e6524620e1c0edc55bdcb0c3197985429366ffb8
MRDC Game Server machine <illinoismrdc@gmail.com>
2019-03-02 17:32:33 -0600

    Deleted dredd-2019 files and updated README

--- client/app/pages/queue/queue.controller.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-angular.module('dreddApp')
-  .controller('QueueCtrl', ['$http', 'gamesService', function($http, gamesService, teamsService) {
-    this.$http = $http;
-    
-    this.gamesService = gamesService;
-    
-  }]);
